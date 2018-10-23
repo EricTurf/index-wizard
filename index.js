75dd2f3bac441055da09f3afe7d7525f66c74a0c
ericturf <eric.turriff@gmail.com>
2018-10-23 12:38:21 -0400

    prettier and recursion?

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node --no-deprecation
 const fs = require('fs');
 const path = require('path');
+const prettier = require('prettier');
 
 const readDir = fs.readdirSync;
 const writeFile = fs.writeFileSync;
+const readFile = fs.readFileSync;
 
 const getComponentName = fileName => {
   const parts = fileName.split('-');
@@ -15,17 +17,62 @@ const getComponentName = fileName => {
 const getIndexContent = fileName => {
   const componentName = getComponentName(fileName);
 
-  return `export { default as ${componentName} } from './${fileName}'`;
+  return `export { default as ${componentName} } from './${fileName}';`;
 };
+const input = process.argv.find(arg => arg.includes('--d='));
 
-const startingDir = (process.argv.find(arg => arg.includes('--d=')) || [])
-  .split('=')
-  .pop();
+if (!input) {
+  console.error('You must give a directory by using the --d= flag');
+  process.exit();
+}
+const startingDir = input.split('=').pop();
 
 const directories = readDir(startingDir);
 
-directories.forEach(dir => {
-  if (dir !== 'index.js') {
-    writeFile(path.join(startingDir, dir, 'index.js'), getIndexContent(dir));
+const getFullPath = filePath => path.join(startingDir, filePath);
+
+const getPretierConfig = () => {
+  try {
+    return JSON.parse(
+      readFile(path.join(process.cwd(), '.prettierrc'), 'utf-8')
+    );
+  } catch (e) {
+    try {
+      return JSON.parse(
+        readFile(path.join(process.cwd(), '.prettierrc.json'), 'utf-8')
+      );
+    } catch (e) {
+      return {
+        singleQuote: true,
+        trailingComma: 'es5',
+      };
+    }
   }
-});
+};
+
+const writeIndexFiles = directoryContent => {
+  directoryContent.forEach(content => {
+    const fullPath = getFullPath(content);
+
+    const isDir = fs.lstatSync(fullPath).isDirectory();
+
+    if (isDir) {
+      const indexFilePath = path.join(fullPath, 'index.js');
+      const dirContent = readDir(fullPath);
+      if (dirContent.includes('index.js')) return;
+      if (
+        dirContent.some(c => fs.lstatSync(path.join(fullPath, c)).isDirectory())
+      )
+        return writeIndexFiles(dirContent);
+
+      writeFile(indexFilePath, getIndexContent(dir));
+      const formattedContent = prettier.format(
+        readFile(indexFilePath, 'utf-8'),
+        Object.assign({}, getPretierConfig(), { parser: 'babylon' })
+      );
+      writeFile(indexFilePath, formattedContent);
+    }
+  });
+};
+
+writeIndexFiles(directories);
