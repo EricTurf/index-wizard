ad3abbfffae526d85d04999b4767d24d9a930bfe
ericturf <eric.turriff@gmail.com>
2018-10-23 09:40:56 -0400

    fixes

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node --no-deprecation
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const readDir = fs.readdirSync;
 const writeFile = fs.writeFileSync;
 
 const getComponentName = fileName => {
-  const parts = fileName.split("-");
+  const parts = fileName.split('-');
   return parts
     .map(part => part.replace(/[a-z]/, part.charAt(0).toUpperCase()))
-    .join();
+    .join('');
 };
 
 const getIndexContent = fileName => {
@@ -18,13 +18,14 @@ const getIndexContent = fileName => {
   return `export { default as ${componentName} } from './${fileName}'`;
 };
 
-const startingDir = process.argv
-  .find(arg => arg.includes("--d="))
-  .split("=")
+const startingDir = (process.argv.find(arg => arg.includes('--d=')) || [])
+  .split('=')
   .pop();
 
 const directories = readDir(startingDir);
 
-directories.forEach(dir =>
-  writeFile(path.join(dir, "index.js"), getIndexContent(dir))
-);
+directories.forEach(dir => {
+  if (dir !== 'index.js') {
+    writeFile(path.join(startingDir, dir, 'index.js'), getIndexContent(dir));
+  }
+});
